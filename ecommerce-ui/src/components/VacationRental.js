59555cfb6804ac6eb479ed1d76489df74e0a3cde
Yavor Papazov <y_papaz@yahoo.com>
2023-02-05 23:25:40 -0500

    complete css

--- ecommerce-ui/src/components/VacationRental.js
+++ ecommerce-ui/src/components/VacationRental.js
@@ -1,14 +1,22 @@
+import classes from "./VacationRental.module.css"
+
 function VacationRental({bnb, addToCart}) {
   return (
-    <div>
-      <div>
+    <div className={classes.container}>
+      <div className={classes["image-div"]}>
         <img src={bnb.image} alt={bnb.title} />
       </div>
-      <h3>{bnb.location.city}, {bnb.location.country}</h3>
-      <h3>{bnb.rating.stars}</h3>
+      <div className={classes.location}>
+        <h3>{bnb.location.city}, {bnb.location.country}</h3>
+        <h3 className={classes.stars}>{bnb.rating.stars}</h3>
+      </div>
       <h3>{bnb.title}</h3>
-      <h3>Cost: ${bnb.payment.cost}</h3>
-      <button onClick={() => addToCart(bnb.id)}>Add to Cart</button>
+      <div className={classes.cost}>
+        <h3>Cost: ${bnb.payment.cost}</h3>
+        <div>
+          <button onClick={() => addToCart(bnb.id)}>Add to Cart</button>
+        </div>
+      </div>
     </div>
   )
 }
